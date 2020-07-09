3f6a6fbfb9262c73fb9acc7963b453665268d72d
fernando.santos <fernando.js8@outlook.com>
2020-07-09 15:37:12 -0300

    Done Log

--- Testes.NetCore.Data/Repository/VksProcess.cs
+++ Testes.NetCore.Data/Repository/VksProcess.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Globalization;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Testes.NetCore.Domain.Interface;
@@ -22,22 +22,48 @@ namespace Testes.NetCore.Data.Repository
             var readDirectory = Path.Combine(baseDirectory, "Read_Directory");
             var logResultsDirectory = Path.Combine(baseDirectory, "Log_Results");
             var processedDirectory = Path.Combine(baseDirectory, "Processed");
-            var resultsFileName = $"results_{DateTime.Now:yyyyMMss}.log";
+            var resultsFileName = $"results_{DateTime.Now:yyyyMMdd}.csv";
             var resultsFileFullPath = Path.Combine(logResultsDirectory, resultsFileName);
 
-            Directory.CreateDirectory(baseDirectory);
+            Directory.CreateDirectory(readDirectory);
+            Directory.CreateDirectory(processedDirectory);
+            Directory.CreateDirectory(logResultsDirectory);
 
             var files = Directory.GetFiles(readDirectory);
 
             foreach (var file in files.Where(f => f.Contains("_log_", StringComparison.InvariantCultureIgnoreCase)))
             {
-                var fileDirectory = Path.Combine(readDirectory, file);
-                var data = _log.Get<VksWgn>(fileDirectory);
+                var data = _log.Get<VksWgn>(file);
 
+                WriteResults(resultsFileFullPath, data);
+
+                File.Move(file, Path.Combine(processedDirectory, Path.GetFileName(file)), true);
             }
 
-
             return true;
+        }
+
+        private static void WriteResults(string resultsFileFullPath, List<VksWgn> data)
+        {
+            foreach (var item in data.Where(d => d.ExceptionThrown || !string.IsNullOrWhiteSpace(d.ExceptionMessage)))
+            {
+                if (!File.Exists(resultsFileFullPath))
+                {
+                    File.WriteAllLines(
+                        resultsFileFullPath,
+                        new List<string>() { "Id;CustomerName;CustomerId;CustomerEmail;CustomerPhone;LeadSource;CreatedDate;HasErrors;ExceptionThrown;ErrorMessage;ExceptionMessage;Message" });
+
+                    File.AppendAllLines(
+                        resultsFileFullPath,
+                        new List<string>() { $"{item.Id};{item.CustomerName};{item.CustomerId};{item.CustomerEmail};{item.CustomerPhone};{item.LeadSource};{item.CreatedDate};{item.HasErrors};{item.ExceptionThrown};{item.ErrorMessage};{item.ExceptionMessage};{item.Message}" });
+                }
+                else
+                {
+                    File.AppendAllLines(
+                        resultsFileFullPath,
+                        new List<string>() { $"{item.Id};{item.CustomerName};{item.CustomerId};{item.CustomerEmail};{item.CustomerPhone};{item.LeadSource};{item.CreatedDate};{item.HasErrors};{item.ExceptionThrown};{item.ErrorMessage};{item.ExceptionMessage};{item.Message}" });
+                }
+            }
         }
     }
 }
--- Testes.NetCore.Domain/Model/VksWgn.cs
+++ Testes.NetCore.Domain/Model/VksWgn.cs
@@ -4,14 +4,14 @@ namespace Testes.NetCore.Domain.Model
 {
     public class VksWgn
     {
-        public int Id { get; }
+        public long Id { get; set; }
         public string CustomerName { get; set; }
-        public int CustomerId { get; set; }
+        public long CustomerId { get; set; }
         public string CustomerEmail { get; set; }
         public long CustomerPhone { get; set; }
         public string LeadSource { get; set; }
         public DateTime LeadCreatedDate { get; set; }
-        public DateTime CreatedDate { get; }
+        public DateTime CreatedDate { get; set; }
         public bool HasErrors { get; set; }
         public bool ExceptionThrown { get; set; }
         public string ErrorMessage { get; set; }
